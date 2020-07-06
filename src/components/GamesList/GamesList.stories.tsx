bb1e28d744dbd3bd3a7db568ef1882bfb1ea7916
Edward Nolan <nolanee@umich.edu>
2020-07-06 00:29:04 -0400

    made some more edits from review

--- src/components/GamesList/GamesList.stories.tsx
+++ src/components/GamesList/GamesList.stories.tsx
@@ -1,36 +1,25 @@
 import React from "react";
 import { GameListEntry } from "./index";
-import GamesList from "."
-import { Segment} from "semantic-ui-react";
+import GamesList from ".";
+import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 export default {
-	title: "Games List Story",
-	component: GamesList,
+  title: "Games List Story",
+  component: GamesList,
 };
 
 const gamesListMock: GameListEntry[] = [
-    {   gameID: 2,
-        playerNames: [
-            "Pumpkin",
-            "Ol' Yeller",
-            "Ol' Smelly",
-            "Lassie"
-        ]
-    },
-    {   gameID: 5,
-        playerNames: [
-            "Mr. Peanutbutter",
-        ]
-   }
+  { gameID: 2, playerNames: ["Pumpkin", "Ol' Yeller", "Ol' Smelly", "Lassie"] },
+  { gameID: 5, playerNames: ["Mr. Peanutbutter"] },
 ];
 
 const handleJoinMock = () => {
-    alert("handleJoin succeeded")
-}
+  alert("handleJoin succeeded");
+};
 
 export const GameListStory = () => (
-    <Segment>
-	    <GamesList games={gamesListMock} onJoin={handleJoinMock}></GamesList>
-    </Segment>
+  <Container>
+    <GamesList games={gamesListMock} onJoin={handleJoinMock}></GamesList>
+  </Container>
 );
